f20b707832d7bfaa16f84faf742df3c51d13ce08
Elisanov Nikolay <Afterlaiv1@yandex.ru>
2024-10-24 16:42:33 +0300

    add function

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+const getLengthString = (string, length) => string.length <= length;
+//==
+const validationPolydrome = (string) => {
+  let result = '';
+  string = string.toLowerCase().replaceAll (' ', '');
+  for(let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return (result === string);
+};
+// ==
+const getStringToNmber = (string) => {
+  let result = '';
+  string = string.toLowerCase().replaceAll (' ', '');
+  for(let i = 0; i < string.length; i++) {
+    if(Number.isNaN(parseInt(string[i], 10))){
+      continue;
+    }
+    result += parseInt(string[i], 10);
+  }
+  return parseInt(result, 10);
+};
+
+// getStringToNmber работает только с аргументом типа: строка
